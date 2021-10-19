d8580c95c89faddf524997753e2488ec9cc6c443
sadrayan <sadrayan@gmail.com>
2021-10-19 00:30:55 -0400

    version

--- devices/deployment.sh
+++ devices/deployment.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-python3 -m venv env
+python -m venv env
 source env/bin/activate
 pip install -r requirements.txt
 mkdir -p deployment
-cd env/lib/python3.*/site-packages/
+cd env/lib/python3.8/site-packages/
 zip -r9 ${OLDPWD}/deployment/rpi-package.zip .
 cd $OLDPWD
 zip -g deployment/rpi-package.zip *.py config.ini resources/*.pem
