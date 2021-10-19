5f8c9a51e2fc1d656ea42b5158df665421f248a3
sadrayan <sadrayan@gmail.com>
2021-10-19 00:33:16 -0400

    echo

--- devices/deployment.sh
+++ devices/deployment.sh
@@ -8,3 +8,4 @@ cd env/lib/python3.8/site-packages/
 zip -r9 ${OLDPWD}/deployment/rpi-package.zip .
 cd $OLDPWD
 zip -g deployment/rpi-package.zip *.py config.ini resources/*.pem
+echo "done creating deployment"
