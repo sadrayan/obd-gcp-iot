67658a5d68b5828fc47c1317fd5e1af3eca1ff45
sadrayan <sadrayan@gmail.com>
2021-10-18 23:01:48 -0400

    python v agnostic

--- devices/deployment.sh
+++ devices/deployment.sh
@@ -3,7 +3,7 @@
 python3 -m venv env
 source env/bin/activate
 pip install -r requirements.txt
-cd env/lib/python3.7/site-packages/
+cd env/lib/python3.*/site-packages/
 zip -r9 ${OLDPWD}/deployment/rpi-package.zip .
 cd $OLDPWD
 zip -g deployment/rpi-package.zip *.py config.ini resources/*.pem
