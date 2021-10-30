501aa4c5ea25c2a7d21db52c91e21471221be186
sadrayan <sadrayan@gmail.com>
2021-10-30 17:40:27 -0400

    pubsub id update

--- terraform/dataflow.tf
+++ terraform/dataflow.tf
@@ -5,7 +5,7 @@ resource "google_dataflow_job" "telemetry_to_bq_job" {
   template_gcs_path = "gs://dataflow-templates-us-central1/latest/PubSub_to_BigQuery"
   temp_gcs_location = "${google_storage_bucket.bucket_data.url}/tmp"
   parameters = {
-    inputTopic      = google_pubsub_topic.pubsub.id
+    inputTopic      = google_pubsub_topic.iot_telemetry.id
     outputTableSpec = "${var.project_name}:${google_bigquery_table.device_data.dataset_id}.${google_bigquery_table.device_data.table_id}"
   }
 }
