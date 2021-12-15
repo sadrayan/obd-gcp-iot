f660cf6654619cdcb24bcfab6f40e18d4b0464d1
sadrayan <sadrayan@gmail.com>
2021-12-15 10:50:12 -0500

    remove http trigger

--- terraform/function.tf
+++ terraform/function.tf
@@ -25,7 +25,6 @@ resource "google_cloudfunctions_function" "function" {
   available_memory_mb   = 1024
   source_archive_bucket = google_storage_bucket.bucket.name
   source_archive_object = google_storage_bucket_object.zip.name
-  trigger_http          = true
   entry_point           = "listDevice"
   event_trigger {
     event_type = "google.storage.object.finalize"
