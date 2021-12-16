5d9a3d3e847c944baa4e20174a839be00cb0b97f
sadrayan <sadrayan@gmail.com>
2021-12-16 10:54:12 -0500

    remove dataflow and bq

--- terraform/bigquery.tf
+++ terraform/bigquery.tf
@@ -1,22 +1,22 @@
-resource "google_bigquery_dataset" "dataset" {
-  dataset_id    = "cvt_dataset"
-  friendly_name = "cvt_iot"
-  location      = "US"
-  description   = "CVT IOT dataset"
+# resource "google_bigquery_dataset" "dataset" {
+#   dataset_id    = "cvt_dataset"
+#   friendly_name = "cvt_iot"
+#   location      = "US"
+#   description   = "CVT IOT dataset"
 
-  default_table_expiration_ms = 3600000
+#   default_table_expiration_ms = 3600000
 
-  labels = {
-    env = "cvt_iot"
-  }
-}
+#   labels = {
+#     env = "cvt_iot"
+#   }
+# }
 
 
-resource "google_bigquery_table" "device_data" {
-  dataset_id = google_bigquery_dataset.dataset.dataset_id
-  table_id   = "cvt_device_data"
-  schema     = file("${path.module}/schema.json")
-  labels = {
-    env = "cvt_iot"
-  }
-}
+# resource "google_bigquery_table" "device_data" {
+#   dataset_id = google_bigquery_dataset.dataset.dataset_id
+#   table_id   = "cvt_device_data"
+#   schema     = file("${path.module}/schema.json")
+#   labels = {
+#     env = "cvt_iot"
+#   }
+# }
--- terraform/dataflow.tf
+++ terraform/dataflow.tf
@@ -1,11 +1,11 @@
-resource "google_dataflow_job" "telemetry_to_bq_job" {
-  name              = "telemetry_to_bq_cvt_iot"
-  max_workers       = 1
-  on_delete         = "cancel"
-  template_gcs_path = "gs://dataflow-templates-us-central1/latest/PubSub_to_BigQuery"
-  temp_gcs_location = "${google_storage_bucket.bucket_data.url}/tmp"
-  parameters = {
-    inputTopic      = google_pubsub_topic.iot_telemetry.id
-    outputTableSpec = "${var.project_id}:${google_bigquery_table.device_data.dataset_id}.${google_bigquery_table.device_data.table_id}"
-  }
-}
+# resource "google_dataflow_job" "telemetry_to_bq_job" {
+#   name              = "telemetry_to_bq_cvt_iot"
+#   max_workers       = 1
+#   on_delete         = "cancel"
+#   template_gcs_path = "gs://dataflow-templates-us-central1/latest/PubSub_to_BigQuery"
+#   temp_gcs_location = "${google_storage_bucket.bucket_data.url}/tmp"
+#   parameters = {
+#     inputTopic      = google_pubsub_topic.iot_telemetry.id
+#     outputTableSpec = "${var.project_id}:${google_bigquery_table.device_data.dataset_id}.${google_bigquery_table.device_data.table_id}"
+#   }
+# }
