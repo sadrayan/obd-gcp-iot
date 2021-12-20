88327fde22c6882fa80dba2166616de971d38034
sadrayan <sadrayan@gmail.com>
2021-12-20 18:51:11 -0500

    deployment package name

--- devices/deployment.sh
+++ devices/deployment.sh
@@ -5,7 +5,7 @@ source env/bin/activate
 pip install -r requirements.txt
 mkdir -p deployment
 cd env/lib/python3.8/site-packages/
-zip -r9 ${OLDPWD}/deployment/rpi-package.zip .
+zip -r9 ${OLDPWD}/deployment/rpi-package.zip#$1 .
 cd $OLDPWD
-zip -g deployment/rpi-package.zip *.py config.ini resources/*.pem
+zip -g deployment/rpi-package.zip#$1 *.py config.ini resources/*.pem
 echo "done creating deployment" && pwd
