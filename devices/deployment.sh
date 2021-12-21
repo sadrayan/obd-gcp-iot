a4e4a10b5abdecca36cea498f9ef837b5fab1d88
sadrayan <sadrayan@gmail.com>
2021-12-20 20:04:36 -0500

    file name with date

--- devices/deployment.sh
+++ devices/deployment.sh
@@ -5,7 +5,8 @@ source env/bin/activate
 pip install -r requirements.txt
 mkdir -p deployment
 cd env/lib/python3.8/site-packages/
-zip -r9 ${OLDPWD}/deployment/rpi-package.zip#$1 .
+date=$(date '+%Y_%m_%d_%H:%M:%S')
+zip -r9 ${OLDPWD}/deployment/rpi-package.zip#$date .
 cd $OLDPWD
-zip -g deployment/rpi-package.zip#$1 *.py config.ini resources/*.pem
+zip -g deployment/rpi-package.zip#$date *.py config.ini resources/*.pem
 echo "done creating deployment" && pwd
