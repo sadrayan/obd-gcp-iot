f89fef078abe4bdb4314fc153ec510a577f55aef
sadrayan <sadrayan@gmail.com>
2021-12-20 20:53:53 -0500

    bucket path name

--- devices/deployment.sh
+++ devices/deployment.sh
@@ -5,8 +5,7 @@ source env/bin/activate
 pip install -r requirements.txt
 mkdir -p deployment
 cd env/lib/python3.8/site-packages/
-date=$(date '+%Y_%m_%d_%H:%M:%S')
-zip -r9 ${OLDPWD}/deployment/rpi-package.zip#$date .
+zip -r9 ${OLDPWD}/deployment/rpi-package.zip .
 cd $OLDPWD
-zip -g deployment/rpi-package.zip#$date *.py config.ini resources/*.pem
+zip -g deployment/rpi-package.zip *.py config.ini resources/*.pem
 echo "done creating deployment" && ls deployment/ && pwd
